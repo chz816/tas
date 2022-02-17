e6c31e44e4c760a66b12769e7f8ca9b318664b1a
tdtrinh11 <tdtrinh11@gmail.com>
2022-02-17 11:51:06 +0700

    fix beginning token

--- model.py
+++ model.py
@@ -517,8 +517,9 @@ class TAASForConditionalGeneration(PegasusPreTrainedModel):
         }
 
     def adjust_logits_during_generation(self, logits, cur_len, max_length):
-        if cur_len == 1 and self.config.force_bos_token_to_be_generated:
-            self._force_token_id_to_be_generated(logits, self.config.bos_token_id)
+        # if cur_len == 1 and self.config.force_bos_token_to_be_generated:
+        if cur_len == 1:
+            self._force_token_id_to_be_generated(logits, self.config.decoder_start_token_id)
         elif cur_len == max_length - 1 and self.config.eos_token_id is not None:
             self._force_token_id_to_be_generated(logits, self.config.eos_token_id)
         return logits
