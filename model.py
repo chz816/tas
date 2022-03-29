b0f2e7c2d6d0c42f4ddb3a412615fb719a388931
tdtrinh11 <tdtrinh11@gmail.com>
2022-03-29 15:00:28 +0700

    .

--- model.py
+++ model.py
@@ -229,7 +229,7 @@ PEGASUS_INPUTS_DOCSTRING = r"""
     PEGASUS_START_DOCSTRING,
 )
 class TAASModel(PegasusPreTrainedModel):
-    def __init__(self, config: PegasusConfig, topic_num=1024, t_vocab_size=2000):
+    def __init__(self, config: PegasusConfig, topic_num=100, t_vocab_size=2000):
         super().__init__(config)
 
         padding_idx, vocab_size = config.pad_token_id, config.vocab_size
@@ -246,7 +246,8 @@ class TAASModel(PegasusPreTrainedModel):
                                           hidden_sizes=(100, 100), activation='relu',
                                           dropout=self.config.dropout, learn_priors=True)
         # transfer the topic modeling vocab to vocab size
-        # self.tm_head = nn.Linear(t_vocab_size, self.shared.num_embeddings, bias=False)
+        self.tm_head = nn.Linear(t_vocab_size, config.d_model, bias=False)
+        self.lm_head = nn.Linear(config.d_model, topic_num, bias=False)
         # for model analysis: use an additional NN to transfer dimension
         # self.dimhead = nn.Linear(config.d_model, self.topic_num, bias=False)
 
@@ -333,16 +334,19 @@ class TAASModel(PegasusPreTrainedModel):
         # self.lm_head = nn.Linear(config.d_model, self.model.shared.num_embeddings, bias=False)
         # self.lm_head(outputs[0]): torch.Size([bs, #(summary), #(vocab)])
         
-        # self.topic_model.topic_word: torch.Size([1024, 2000]) (torch.Size([num_topics, vocab_size]))
-        # self.tm_head = nn.Linear(vocab_size, self.model.shared.num_embeddings, bias=False)
-        # self.tm_head(self.topic_model.topic_word): torch.Size([bs, num_topics, vocab_size])
+        # self.topic_model.topic_word: torch.Size([1024, 2000]) (torch.Size([num_topics, #(t_vocab)]))
+        # self.tm_head = nn.Linear(#(t_vocab), self.model.shared.num_embeddings, bias=False)
+        # self.tm_head(self.topic_model.topic_word): torch.Size([bs, num_topics, #(vocab)])
         # torch.matmul() : torch.Size([bs, #(summary), #(vocab)])
 
         # lm_logits.size(): torch.Size([16, 38, 50264]) => batch_size * #(summary) * len(vocab)
         # theta = self.topic_model.get_theta(bow, outputs.encoder_last_hidden_state[::, 0])
 
+        # encoder_outputs[0]: [bs, #doc, d_model]
+
         if topic_guided:
-            _encoder_hidden_states = encoder_outputs[0] + self.topic_model.topic_word
+            # convert encoder_outputs[0] to 
+            _encoder_hidden_states = encoder_outputs[0] + torch.matmul(self.lm_head(encoder_outputs[0]), self.tm_head(self.topic_model.topic_word))
         else:
             _encoder_hidden_states = encoder_outputs[0]
 
@@ -395,7 +399,7 @@ class TAASForConditionalGeneration(PegasusPreTrainedModel):
         r"lm_head\.weight",
     ]
 
-    def __init__(self, config: PegasusConfig, topic_num=1024, t_vocab_size=2000):
+    def __init__(self, config: PegasusConfig, topic_num=100, t_vocab_size=2000):
         super().__init__(config)
         self.model = TAASModel(config, topic_num=topic_num, t_vocab_size=t_vocab_size)
         self.register_buffer("final_logits_bias", torch.zeros((1, self.model.shared.num_embeddings)))
@@ -494,9 +498,9 @@ class TAASForConditionalGeneration(PegasusPreTrainedModel):
         # self.lm_head = nn.Linear(config.d_model, self.model.shared.num_embeddings, bias=False)
         # self.lm_head(outputs[0]): torch.Size([bs, #(summary), #(vocab)])
         
-        # self.topic_model.topic_word: torch.Size([1024, 2000]) (torch.Size([bs, num_topics, vocab_size]))
-        # self.tm_head = nn.Linear(vocab_size, self.model.shared.num_embeddings, bias=False)
-        # self.tm_head(self.topic_model.topic_word): torch.Size([bs, num_topics, vocab_size])
+        # self.topic_model.topic_word: torch.Size([1024, 2000]) (torch.Size([num_topics, #(t_vocab)]))
+        # self.tm_head = nn.Linear(#(t_vocab), #(vocab), bias=False)
+        # self.tm_head(self.topic_model.topic_word): torch.Size([num_topics, #(t_vocab)])
         # torch.matmul() : torch.Size([bs, #(summary), #(vocab)])
 
         # lm_logits.size(): torch.Size([16, 38, 50264]) => batch_size * #(summary) * len(vocab)
